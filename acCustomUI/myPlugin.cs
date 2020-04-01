ac55083190f59dbed631f483882e4d488e05659d
utop <utop@163.com>
2020-04-01 11:15:25 +0800

    每个步骤完成输出日志

--- acCustomUI/myPlugin.cs
+++ acCustomUI/myPlugin.cs
@@ -43,36 +43,46 @@ namespace AutoCAD_CSharp_plug_in_acCustomUI
             ////MessageBox.Show("插件已加载");
             ///
             Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
-            editor.WriteMessage("MyPlugin插件初始化");
+            editor.WriteMessage("MyPlugin插件初始化\n");
 
             //TODO 处理工作区的 tab - 应不可关闭
 
             //TODO 处理左上角图标和主菜单
             //MyCommands.ZsyMainMenu();
 
-            //隐藏AutoCAD自带 toolbar，添加自定义toolbar
-            MyCommands.ZsyToolbar();
-
+            
             //无选定对象时的右键菜单
             MyCommands.ZsyAddDefaultContextMenuItem();
 
+            editor.WriteMessage("1 完成 MyCommands.ZsyAddDefaultContextMenuItem()\n");
+
             //有选定对象时的右键菜单
             MyCommands.ZsyAddObjectContextMenuItem();
+            editor.WriteMessage("2  MyCommands.ZsyAddObjectContextMenuItem()\n");
             
             //顶部Ribbon 菜单区域，AutoCAD自带tab隐藏，添加自定义tab、按钮
             MyCommands.ZsyRibbonTab();
-
+            editor.WriteMessage("3  MyCommands.ZsyRibbonTab()\n");
             //
 
             //上下左右添加自定义停靠区域
             MyCommands.ZsyPaletteSetLeft();  //包含弹出模态窗口、非模态置顶窗口
+            editor.WriteMessage("4  MyCommands.ZsyPaletteSetLeft()\n");
             MyCommands.ZsyPaletteSetTop();
+            editor.WriteMessage("5  MyCommands.ZsyPaletteSetTop()\n");
             MyCommands.ZsyPaletteSetBottom();
+            editor.WriteMessage("6  MyCommands.ZsyPaletteSetBottom()\n");
             MyCommands.ZsyPaletteSetRight();  //WPF UserControl
+            editor.WriteMessage("7  MyCommands.ZsyPaletteSetRight()\n");
 
             //监听选定事件，选定对象id 在 非模态置顶窗口中显示
             MyEvent.AddDocEvent();
+            editor.WriteMessage("8  MyEvent.AddDocEvent()\n");
 
+
+            //隐藏AutoCAD自带 toolbar，添加自定义toolbar
+            MyCommands.ZsyToolbar();
+            editor.WriteMessage("9  MyCommands.ZsyToolbar()\n");
         }
 
         void IExtensionApplication.Terminate()
